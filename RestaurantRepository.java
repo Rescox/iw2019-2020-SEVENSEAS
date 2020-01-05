3c19d2e99431de97b90edce0ec89c92c2d03bca6
Claudia <56028837+ghost-ctrl@users.noreply.github.com>
2020-01-05 03:01:45 +0100

    Add files via upload

---
+++ RestaurantRepository.java
@@ -0,0 +1,17 @@
+package es.uca.iw.sss.spring;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant,Long> {
+
+
+
+    Restaurant findById(int id);
+
+    List<Restaurant> findAll();
+
+
+}
