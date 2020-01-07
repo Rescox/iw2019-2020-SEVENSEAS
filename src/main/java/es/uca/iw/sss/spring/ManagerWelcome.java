2283b311ab3f47516387ff3069881fa87716791c
Raúl <38223132+Rescox@users.noreply.github.com>
2020-01-07 10:31:58 +0100

    Add files via upload

--- src/main/java/es/uca/iw/sss/spring/ManagerWelcome.java
+++ src/main/java/es/uca/iw/sss/spring/ManagerWelcome.java
@@ -7,6 +7,9 @@ import com.vaadin.flow.component.charts.model.ChartType;
 import com.vaadin.flow.component.charts.model.Configuration;
 import com.vaadin.flow.component.charts.model.ListSeries;
 import com.vaadin.flow.component.charts.model.XAxis;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
@@ -14,23 +17,42 @@ import com.vaadin.flow.router.Route;
 public class ManagerWelcome extends VerticalLayout {
     private static final String[] MONTH_LABELS = new String[] {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
             "Aug", "Sep", "Oct", "Nov", "Dec"};
-    public ManagerWelcome() {
+    private ShipRepository shipRepository;
+    private Grid shipGrid;
+    private Ship ship;
 
-        Chart chart = new Chart(ChartType.COLUMN);
+    public ManagerWelcome(ShipRepository shipRepository) {
+        this.shipRepository = shipRepository;
+        this.shipGrid = new Grid<>(Ship.class);
+        shipGrid.setItems(shipRepository.findAll());
+        shipGrid.setColumns("id","name","licensePlate");
+        shipGrid.asSingleSelect().addValueChangeListener(e -> {
+            ManagerWelcome.addChart((Ship) e.getValue(), this);
+             ship = (Ship) e.getValue(); // Barco seleccionado
+        });
+        add(shipGrid);
+    }
+
+    public static void addChart(Ship ship, ManagerWelcome managerWelcome) {
+        Chart chart = new Chart(ChartType.SPLINE);
         Configuration conf = chart.getConfiguration();
-        conf.setTitle("HOla");
+        conf.setTitle(ship.getName());
         ListSeries series = new ListSeries("Diameter");
         series.setData(4900,  12100,  12800,
                 6800,  143000, 125000,
                 51100, 49500);
+        Number[] money = series.getData();
+        float totalMoney = 0;
+        for(Number i : money) {
+            totalMoney += i.floatValue();
+        }
+        H1 facturation = new H1("El número total de facturación este año es de: " + totalMoney + "€");
         conf.addSeries(series);
         XAxis xaxis = new XAxis();
         xaxis.setCategories(MONTH_LABELS);
         xaxis.setTitle("Months");
         conf.addxAxis(xaxis);
-        conf.setSubTitle("buenas tardes");
-        add(chart);
-
-
+        conf.setSubTitle(ship.getLicensePlate());
+        managerWelcome.add(chart, facturation);
     }
 }
