2ac1772bf4d1d3b59981be41107e287e6fc38c59
Kevin López <kevin.lopezcala99@gmail.com>
2020-01-10 00:09:15 +0100

    Delete ScaleRepository.java

--- ScaleRepository.java
+++
@@ -1,8 +0,0 @@
-package es.uca.iw.sss.spring;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScaleRepository extends JpaRepository<Scale,Long> {
-    Scale findById(int id);
-
-}
