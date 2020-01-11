b47d4e143ab54e8b55262416c16976feca4216af
Kevin López <kevin.lopezcala99@gmail.com>
2020-01-11 22:06:42 +0100

    Add files via upload

--- src/main/java/es/uca/iw/sss/spring/RestaurantService.java
+++ src/main/java/es/uca/iw/sss/spring/RestaurantService.java
@@ -5,16 +5,29 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @Service
 public class RestaurantService {
 
-    @Autowired
-    RestaurantRepository restaurantRepository;
+    private RestaurantRepository restaurantRepository;
+    private static RestaurantService stance;
+    private static final Logger LOGGER = Logger.getLogger(RestaurantService.class.getName());
 
+    @Autowired
     public RestaurantService(RestaurantRepository repo) {
+        super();
         this.restaurantRepository = repo;
     }
+
+    /**
+     * @return a reference to an example facade for Customer objects.
+     */
+    public static RestaurantService getInstance() {
+
+        return stance;
+    }
+
 
     public Restaurant saveRestaurant(Restaurant restaurant) {
         return restaurantRepository.save(restaurant);
