d5631e9abd0c32ec04948565bd84f29cc266b29e
Kevin López <kevin.lopezcala99@gmail.com>
2020-01-13 13:58:34 +0100

    Add files via upload

--- src/main/java/es/uca/iw/sss/spring/ShopRepository.java
+++ src/main/java/es/uca/iw/sss/spring/ShopRepository.java
@@ -5,10 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ShopRepository extends JpaRepository<Shop,Long> {
-
-    Shop findById(int id);
-
-    List<Shop> findAll();
-
     List<Shop> findByNameStartsWithIgnoreCase(String name);
 }
