533732f18f908a32afcf4e7af413d86a390e0eb3
Claudia <56028837+ghost-ctrl@users.noreply.github.com>
2020-01-14 14:12:47 +0100

    Add files via upload

--- src/main/java/es/uca/iw/sss/spring/ui/costumer/SpaView.java
+++ src/main/java/es/uca/iw/sss/spring/ui/costumer/SpaView.java
@@ -1,4 +1,4 @@
-package es.uca.iw.sss.spring.ui.costumer;
+package es.uca.iw.sss.spring;
 
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.UI;
@@ -14,24 +14,19 @@ import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.timepicker.TimePicker;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.router.*;
-import es.uca.iw.sss.spring.ServicesView;
-import es.uca.iw.sss.spring.SpaReservation;
-import es.uca.iw.sss.spring.backend.entities.Spa;
-import es.uca.iw.sss.spring.backend.repositories.SpaRepository;
-import es.uca.iw.sss.spring.backend.repositories.UserRepository;
-import es.uca.iw.sss.spring.backend.services.SpaReservationService;
-import es.uca.iw.sss.spring.backend.services.SpaService;
-import es.uca.iw.sss.spring.backend.services.UserService;
-import es.uca.iw.sss.spring.ui.common.MainLayout;
-import es.uca.iw.sss.spring.ui.common.WelcomeView;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static es.uca.iw.sss.spring.utils.SecurityUtils.getUser;
@@ -43,10 +38,11 @@ public class SpaView extends HorizontalLayout implements HasUrlParameter<Long> {
     private TextField firstName = new TextField("First name");
     private TextField lastName = new TextField("Last name");
     DatePicker datePicker;
+    NumberField numberField;
     LocalDate now = LocalDate.now();
     TimePicker timePicker;
 
-    /*private Dialog dialog = new Dialog();*/
+    private SpaReservationRepository spaReservationRepository;
     private SpaRepository spaRepository;
     private SpaReservationService spaReservationService;
     private SpaService spaService;
@@ -62,13 +58,15 @@ public class SpaView extends HorizontalLayout implements HasUrlParameter<Long> {
     private Image img;
 
     @Autowired
-    public SpaView(SpaReservationService spaReservationService, UserService userService, SpaRepository spaRepository, SpaService spaService) {
+    public SpaView(SpaReservationService spaReservationService, UserService userService, SpaRepository spaRepository, SpaService spaService, SpaReservationRepository spaReservationRepository) {
 
         //Pintar parte de registrar reserva
         datePicker = new DatePicker();
         timePicker = new TimePicker();
-        FormLayout formLayout = new FormLayout();
+        numberField = new NumberField();
+
         this.spaReservationService = spaReservationService;
+        this.spaReservationRepository = spaReservationRepository;
         this.userService = userService;
         this.spaRepository = spaRepository;
         this.spaService = spaService;
@@ -95,9 +93,17 @@ public class SpaView extends HorizontalLayout implements HasUrlParameter<Long> {
         timePicker.setLabel("Choose a time");
         timePicker.setMin("13:00");
         timePicker.setMax("23:00");
+        numberField.setValue(1d);
+        numberField.setHasControls(true);
+        numberField.setMin(1);
+        numberField.setLabel("Number of persons");
 
         Button register = new Button("Register", event -> {
-           registerReservation();
+            try {
+                registerReservation();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
         });
         Button cancel = new Button("Cancel",  event -> {
             UI.getCurrent().navigate(ServicesView.class);
@@ -111,22 +117,52 @@ public class SpaView extends HorizontalLayout implements HasUrlParameter<Long> {
         verticalLayout1.setWidth("20%");
         verticalLayout2.setWidth("60%");
         verticalLayout3.setWidth("20%");
-        verticalLayout1.add(name,description,price,firstName, lastName, datePicker,timePicker,buttons);
+        verticalLayout1.add(name,description,price,firstName, lastName, datePicker,timePicker,numberField,buttons);
         verticalLayout3.add(img);
         add(verticalLayout3,verticalLayout1,verticalLayout2);
 
     }
 
-    private void registerReservation() {
+    private boolean enoughAforum() throws ParseException {
+
+        String dateHour1 = spaReservation.getDate() +" "+spaReservation.getHour();
+        Date parsedDateHour1 = new SimpleDateFormat("yyyy-MM-dd hh:mm").parse(dateHour1);
+
+        List<SpaReservation> reservations = spaReservationRepository.findBySpa(spa);
+        Long cont = 0L;
+        for(SpaReservation r: reservations)
+        {
+            String dateHour2 = r.getDate() +" "+r.getHour();
+            Date parsedDateHour2 = new SimpleDateFormat("yyyy-MM-dd hh:mm").parse(dateHour2);
+            if(parsedDateHour1.equals(parsedDateHour2))
+            {
+                cont = cont + r.getPersons();
+            }
+        }
+
+        if(cont + spaReservation.getPersons() > spa.getAforum())
+        {
+            return false;
+        }
+        else return true;
+
+    }
+
+    private void registerReservation() throws ParseException {
         spaReservation.setFirstName(firstName.getValue());
         spaReservation.setLastName(lastName.getValue());
         spaReservation.setUser(getUser());
         spaReservation.setServices("1");
         spaReservation.setDate(datePicker.getValue().toString());
+        spaReservation.setPrice(numberField.getValue().floatValue() * spa.getPrice() );
         spaReservation.setHour(timePicker.getValue().toString());
-        spaReservationService.create(spaReservation);
-        UI.getCurrent().navigate(WelcomeView.class);
-        UI.getCurrent().getPage().reload();
+        spaReservation.setPersons(numberField.getValue().longValue());
+        if(enoughAforum())
+        {
+            spaReservationService.create(spaReservation);
+            UI.getCurrent().navigate(WelcomeView.class);
+            UI.getCurrent().getPage().reload();
+        }
     }
 
     @Override
@@ -140,6 +176,7 @@ public class SpaView extends HorizontalLayout implements HasUrlParameter<Long> {
             spa = spaService.findById(id).get();
             this.name.setText(spa.getName());
             this.description.setText(spa.getDescription());
+            this.price.setText(""+spa.getPrice()+"€ per person");
             this.photourl = spa.getPhoto();
 
         }
