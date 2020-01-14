23ccf0d637576df1376a4afe62e3c8b27fa94116
Claudia <56028837+ghost-ctrl@users.noreply.github.com>
2020-01-14 13:17:10 +0100

    Add files via upload

--- src/main/java/es/uca/iw/sss/spring/backend/repositories/ReservationRepository.java
+++ src/main/java/es/uca/iw/sss/spring/backend/repositories/ReservationRepository.java
@@ -1,9 +1,12 @@
-package es.uca.iw.sss.spring.backend.repositories;
+package es.uca.iw.sss.spring;
 
-import es.uca.iw.sss.spring.backend.entities.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+import java.util.List;
 
-  Reservation findById(int id);
+public interface ReservationRepository extends JpaRepository <Reservation,Long> {
+
+        Reservation findById(int id);
+        List<Reservation> findByRestaurant(Restaurant restaurant);
+
 }
